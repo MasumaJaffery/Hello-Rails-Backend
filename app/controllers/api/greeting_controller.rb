b4fe5cd0bfd7b109daa25d6ff2ffdf4ffb4e7ec0
MasumaJaffery <masumajaffery1@gmail.com>
2023-11-11 22:51:34 +0500

    Resolve

--- app/controllers/api/greeting_controller.rb
+++ app/controllers/api/greeting_controller.rb
@@ -1,7 +1,6 @@
 class Api::GreetingController < ApplicationController
-    def api
-      random_greeting = Greeting.order('RANDOM()').first
-      render json: { message: random_greeting&.message || 'No greetings found' }
-    end
+  def api
+    random_greeting = Greeting.order('RANDOM()').first
+    render json: { message: random_greeting&.message || 'No greetings found' }
   end
-  
+end
